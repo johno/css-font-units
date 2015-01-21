0d6d4833c6457f9d9325fcdfb5683c405f7817fc
John Otander <johnotander@gmail.com>
2015-01-21 11:44:30 -0700

    Initial commit

---
+++ index.js
@@ -0,0 +1,7 @@
+'use strict';
+
+module.exports = function cssFontUnits(options) {
+  options = options || {};
+
+  return true;
+}
---
+++ test/test.js
@@ -0,0 +1,9 @@
+var assert = require('assert');
+var cssFontUnits = require('..');
+
+describe('css-font-units', function() {
+
+  it('should do something awesome', function() {
+    assert.equal(cssFontUnits(), true);
+  });
+});
